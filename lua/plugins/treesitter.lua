ed2036871e8c6b24f3c0d8abdd95479ac4e817bc
FedericoBruzzone <federico.bruzzone.i@gmail.com>
2026-01-08 01:04:34 +0100

    Update treesitter
    Signed-off-by: FedericoBruzzone <federico.bruzzone.i@gmail.com>

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,5 +1,59 @@
+-- return {
+--     "nvim-treesitter/nvim-treesitter",
+--     build = ":TSUpdate",
+-- }
+-- :checkhealth nvim-treesitter
 return {
-    "nvim-treesitter/nvim-treesitter",
-    lazy = false, -- this plugin does not support lazy loading
-    build = ":TSUpdate",
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate",
+        config = function()
+            require("nvim-treesitter.config").setup()
+            require('nvim-treesitter').install({
+                "json",
+                "python",
+                "ron",
+                "javascript",
+                "haskell",
+                "query",
+                "typescript",
+                "tsx",
+                "rust",
+                "zig",
+                "php",
+                "yaml",
+                "html",
+                "css",
+                "markdown",
+                "markdown_inline",
+                "bash",
+                "lua",
+                "vim",
+                "vimdoc",
+                "c",
+                "dockerfile",
+                "gitignore",
+                "astro",
+                "llvm"
+            })
+
+            vim.cmd.syntax("off")
+            vim.api.nvim_create_autocmd("BufReadPost", {
+                pattern = "*",
+                callback = function()
+                    -- can start a specific treesitter on a specific buffer also
+                    -- vim.treesitter.start(0, "c")
+
+                    -- syntax highlighting, provided by Neovim
+                    vim.treesitter.start()
+                    -- folds, provided by Neovim
+                    vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                    vim.wo.foldmethod = 'expr'
+                    -- indentation, provided by nvim-treesitter
+                    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+                end,
+                once = true,
+            })
+        end
+    }
 }
